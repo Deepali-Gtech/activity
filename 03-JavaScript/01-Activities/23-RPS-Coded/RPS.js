98d4bca850a4de2f2640e64cdbaf6801fa7b0bc8
Deepali Garg <deepaligarg.chd@gmail.com>
2020-12-18 19:37:12 -0500

    class activity

---
+++ 03-JavaScript/01-Activities/23-RPS-Coded/RPS.js
@@ -0,0 +1,12 @@
+
+
+var RPS = ["R", "p", "s"]
+
+
+for (var i = 0; i < 10; i++) {
+    // Generate a random decimal number between 0 and 1
+    var num = Math.random();
+
+    // Display in console
+    console.log(RPS);
+}
